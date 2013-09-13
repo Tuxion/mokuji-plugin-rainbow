ac8273d8aea161f3f45a4c8c0ed9ef39e6ca25be
Bart Roorda <post@bartroorda.nl>
2013-09-13 12:11:09 +0200

    Added plugin files.

---
+++ plugin.php
@@ -0,0 +1,5 @@
+<?php namespace plugins; if(!defined('TX')) die('No direct access.'); ?>
+
+  <!-- <link href="<?php echo $plugin; ?>themes/github.css" rel="stylesheet" type="text/css"> -->
+  
+  <script type="text/javascript" src="<?php echo $plugin; ?>rainbow-css-html-js-php.min.js"></script>
